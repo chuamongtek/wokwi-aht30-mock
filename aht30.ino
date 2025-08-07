8118b2f6cae0b76aa8f850e4c3a269e610cc2bda
chuamongtek <chuamongtek@gmail.com>
2025-08-07 19:54:16 +0700

    Create aht30.ino

---
+++ aht30.ino
@@ -0,0 +1,17 @@
+#include <Wire.h>
+void setup() {
+  Wire.begin();
+  Serial.begin(9600);
+}
+void loop() {
+  Wire.requestFrom(0x38, 4);
+  while (Wire.available() < 4);
+  uint16_t h = Wire.read() << 8 | Wire.read();
+  uint16_t t = Wire.read() << 8 | Wire.read();
+  Serial.print("Humidity: ");
+  Serial.print(h / 100.0);
+  Serial.print("%  Temp: ");
+  Serial.print(t / 100.0);
+  Serial.println("°C");
+  delay(2000);
+}
